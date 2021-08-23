e714c036a53afab43be1a8da90bbdf30dfc67d3a
anonymous0120 <57787265+anonymous0120@users.noreply.github.com>
2021-08-23 11:14:51 +0800

    Update run_generate_adv.sh

--- run_generate_adv.sh
+++ run_generate_adv.sh
@@ -4,7 +4,7 @@ INPUT_DIR=/path/to/benign/images
 OUTPUT_DIR=/path/to/output/images
 ATTACK_METHOD=afv
 
-for SOURCE_MODEL in vgg16 resnet152 inception_v3
+for SOURCE_MODEL in vgg16 resnet152
 do
   python3 examples/example_generate_adv.py --input_dir  $INPUT_DIR --output_dir $OUTPUT_DIR --source_model $SOURCE_MODEL --attack_method $ATTACK_METHOD
 done
